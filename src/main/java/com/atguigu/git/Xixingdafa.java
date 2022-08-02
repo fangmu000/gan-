45a106182ef5a49c8c31489f0f146de324dbd97c
zangrenjie <zangrenjie@atguigu.com>
2022-08-03 00:28:48 +0800

    create 吸星大法

---
+++ src/main/java/com/atguigu/git/Xixingdafa.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class Xixingdafa {
+
+    public static void main(String[] args) {
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+    }
+}
